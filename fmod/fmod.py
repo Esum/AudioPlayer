ee675229801fc6fa0fff413503cf8cc6224c86d8
Esum <esum.game@gmail.com>
2016-08-27 09:58:01 +0200

    Added threaded sound play

--- fmod/fmod.py
+++ fmod/fmod.py
@@ -3,30 +3,42 @@ from threading import Thread
 import time
 VERSION = 0x00010810
 
+FMOD = WinDLL("fmodL")
+
+
+class SystemUpdateThread(Thread):
+
+    def __init__(self, fmod_system: c_voidp):
+        super(SystemUpdateThread, self).__init__()
+        self.system = fmod_system
+    
+    def run(self):
+        for _ in range(0, 100):
+            FMOD.FMOD_System_Update(self.system)
+            time.sleep(0.05)
+
+
 class FMOD_System:
 
     def __init__(self):
-        self.dll = WinDLL("fmodL")
+        FMOD = WinDLL("fmodL")
         fmod_system = c_voidp()
-        self.dll.FMOD_System_Create(byref(fmod_system))
-        self.dll.FMOD_System_Init(fmod_system, 6, 0, 0)
+        FMOD.FMOD_System_Create(byref(fmod_system))
+        FMOD.FMOD_System_Init(fmod_system, 6, 0, 0)
         self.system = fmod_system
 
-    def play(self, path):
+    def play(self, path: str) -> SystemUpdateThread:
         sound = c_voidp()
-        self.dll.FMOD_System_CreateStream(self.system, path.encode('ascii'), 0, 0, byref(sound))
+        FMOD.FMOD_System_CreateStream(self.system, path.encode('ascii'), 0, 0, byref(sound))
         num_subsounds = c_int()
-        self.dll.FMOD_Sound_GetNumSubSounds(sound, num_subsounds)
+        FMOD.FMOD_Sound_GetNumSubSounds(sound, num_subsounds)
 
         if num_subsounds.value:
-            threads = []
-            for subsound_num in range(num_subsounds.value):
-                subsound = c_voidp()
-                self.dll.FMOD_Sound_GetSubSound(sound, subsound_num, byref(subsound))
-                self.dll.FMOD_System_PlaySound(self.system, subsound, 0, 0, 0)
-        else:
-            self.dll.FMOD_System_PlaySound(self.system, sound, 0, 0, 0)
+            FMOD.FMOD_Sound_GetSubSound(sound, 0, byref(sound))
         
-        for _ in range(0, 100):
-            self.dll.FMOD_System_Update(self.system)
-            time.sleep(0.050)
+        FMOD.FMOD_System_PlaySound(self.system, sound, 0, 0, 0)
+        
+        update_thread = SystemUpdateThread(self.system)
+        update_thread.start()
+
+        return update_thread
